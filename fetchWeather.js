00f39db84703d4f46cfbd12593cc1927385a7fad
DraytonFHSU <ddpletcher@mail.fhsu.edu>
2023-09-17 20:34:09 -0500

    Fixed a bug where it only took Hays as a value

--- fetchWeather.js
+++ fetchWeather.js
@@ -1,14 +1,11 @@
-const searchKey = "Hays, KS";
 const apiURL = "http://api.weatherapi.com/v1/current.json?key=bfaf6f3581b14ed88fc233827231009&q=";
 const searchBox = document.querySelector(".search input")
 const searchButton = document.querySelector(".search button")
 /* searchKey can either be a zip code or City, State*/
 
 async function checkWeather(City){
-    const response = await fetch(apiURL + searchKey + "&aqi=no");
+    const response = await fetch(apiURL + City + "&aqi=no");
     var data = await response.json();
-
-    console.log(data);
 
     document.querySelector(".city").innerHTML = data.location.name;
     document.querySelector(".temp").innerHTML = Math.round(data.current.temp_c);
